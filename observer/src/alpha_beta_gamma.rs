3cc15cdd343541b8c1a9e41b3951e7b0aa3dbb90
Sofia Silvestri <sofia.silvestri@glaukonis.com>
2025-12-05 12:57:03 +0100

    Planned work for SignalProcessing

---
+++ observer/src/alpha_beta_gamma.rs
@@ -0,0 +1,49 @@
+use std::collections::HashMap;
+use std::any::Any;
+use std::fmt::Display;
+use std::sync::mpsc::SyncSender;
+use std::sync::{Arc, Mutex};
+use serde::Serialize;
+use stream_proc_macro::{StreamBlockMacro};
+use data_model::streaming_data::{StreamingError, StreamingState};
+use data_model::memory_manager::{DataTrait, StaticsTrait, State, Parameter, Statics};
+use processor_engine::stream_processor::{StreamBlock, StreamBlockDyn, StreamProcessor};
+use processor_engine::connectors::{ConnectorTrait, Input, Output};
+
+#[derive(StreamBlockMacro)]
+pub struct alpha_beta_gamma {
+    name:       &'static str,
+    inputs:     HashMap<&'static str, Box<dyn ConnectorTrait>>,
+    outputs:    HashMap<&'static str, Box<dyn ConnectorTrait>>,
+    parameters: HashMap<&'static str, Box<dyn DataTrait>>,
+    statics:    HashMap<&'static str, Box<dyn StaticsTrait>>,
+    state:      HashMap<&'static str, Box<dyn DataTrait>>,
+    lock:       Arc<Mutex<()>>,
+    proc_state: Arc<Mutex<StreamingState>>,
+}
+impl alpha_beta_gamma {
+    pub fn new(name: &'static str) -> Self {
+        let mut ret = Self {
+            name,
+            inputs: HashMap::new(),
+            outputs: HashMap::new(),
+            parameters: HashMap::new(),
+            statics: HashMap::new(),
+            state: HashMap::new(),
+            lock: Arc::new(Mutex::new(())),
+            proc_state: Arc::new(Mutex::new(StreamingState::Null)),
+        };
+
+        ret
+    }
+}
+impl StreamProcessor for alpha_beta_gamma {
+    fn init(&mut self) -> Result<(), StreamingError> {
+    }
+    fn run(&mut self) -> Result<(), StreamingError> {
+    }
+    fn process(&mut self) -> Result<(), StreamingError> {
+    }
+    fn stop(&mut self) -> Result<(), StreamingError> {
+    }
+}
